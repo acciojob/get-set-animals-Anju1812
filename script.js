2b72a420038ea725e5ffef1e0755c298c3ebfe2f
Anju1812 <117847428+Anju1812@users.noreply.github.com>
2023-07-23 20:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log("Some generic sound");
+  }
+}
+
+class Cat extends Animal {
+  constructor(species) {
+    // Call the parent class (Animal) constructor
+    super(species);
+  }
+
+  // Method specific to Cat class
+  purr() {
+    console.log("purr");
+  }
+
+  // Override the makeSound() method of the parent class
+  makeSound() {
+    console.log("Meow!");
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+    // Call the parent class (Animal) constructor
+    super(species);
+  }
+
+  // Method specific to Dog class
+  bark() {
+    console.log("woof");
+  }
+
+  // Override the makeSound() method of the parent class
+  makeSound() {
+    console.log("Woof!");
+  }
+}
+
+// Example usage:
+const genericAnimal = new Animal("Unknown");
+console.log("Species:", genericAnimal.species); // Output: Species: Unknown
+genericAnimal.makeSound(); // Output: Some generic sound
+
+const cat = new Cat("Felis catus");
+console.log("Species:", cat.species); // Output: Species: Felis catus
+cat.makeSound(); // Output: Meow!
+cat.purr(); // Output: purr
+
+const dog = new Dog("Canis lupus familiaris");
+console.log("Species:", dog.species); // Output: Species: Canis lupus familiaris
+dog.makeSound(); // Output: Woof!
+dog.bark(); // Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
